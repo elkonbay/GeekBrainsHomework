36168896c014d0df6d1ce216d3ed44e5eea0195f
Elmur Konushbaev <el.konushbaev@gmail.com>
2023-02-25 15:37:25 +0800

    Палиндром

---
+++ 19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0] == number[4] || number[1] == number[3]){
+    Console.WriteLine("палиндром");
+  }
+  else Console.WriteLine("не палиндром");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"не правильное число");
